da7f8d5743c325b2871e23feb2005b9824f3099b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-26 00:23:09 +0900

    Setup OpenVPN Client

---
+++ provision/openvpn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+#apt-get install -y openvpn
+for crt in ca.crt zbx3.crt; do
+  install -D -m644 "/vagrant/tmp/openvpn/$crt" "/etc/openvpn/keys/$crt"
+done
+for key in ta.key zbx3.key; do
+  install -D -m600 "/vagrant/tmp/openvpn/$key" "/etc/openvpn/keys/$key"
+done
+install -D -m644 "/vagrant/tmp/openvpn/client.conf" "/etc/openvpn/client.conf"
+systemctl enable openvpn@client
+systemctl start openvpn@client
+ufw allow out proto udp to "$(getent hosts ns6.n-z.jp | awk '{print $1}')" port "$(awk '$1=="remote"{print $3}' /etc/openvpn/client.conf)"
+#etckeeper commit "Setup OpenVPN" || :
