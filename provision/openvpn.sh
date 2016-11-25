3166b0c68dca4b6be4052313ef3a872ac6e73673
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-26 00:30:41 +0900

    Uncomment some lines

--- provision/openvpn.sh
+++ provision/openvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-#apt-get install -y openvpn
+apt-get install -y openvpn
 for crt in ca.crt zbx3.crt; do
   install -D -m644 "/vagrant/tmp/openvpn/$crt" "/etc/openvpn/keys/$crt"
 done
@@ -11,4 +11,4 @@ install -D -m644 "/vagrant/tmp/openvpn/client.conf" "/etc/openvpn/client.conf"
 systemctl enable openvpn@client
 systemctl start openvpn@client
 ufw allow out proto udp to "$(getent hosts ns6.n-z.jp | awk '{print $1}')" port "$(awk '$1=="remote"{print $3}' /etc/openvpn/client.conf)"
-#etckeeper commit "Setup OpenVPN" || :
+etckeeper commit "Setup OpenVPN" || :
