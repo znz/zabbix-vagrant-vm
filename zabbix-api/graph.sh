11556a7c8d46b5f4baf93fc34cbd5c08402d4aa3
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-26 01:07:11 +0900

    Skip if empty

--- zabbix-api/graph.sh
+++ zabbix-api/graph.sh
@@ -13,8 +13,10 @@ upsert_graph "0 LDAPS Cert Remaining" $(echo "$JSON" | jq -r '.result[].itemid')
 
 zabbix_api host.get '{"output":"extend"}'
 host_json="$JSON"
-for num in 1 2 3; do
+for num in 1 2 3 4; do
     hostids=$(echo "$host_json" | jq -r '.result | map(select(.host | startswith("'"$num"' ")) | .hostid)')
-    zabbix_api item.get '{"output":"extend","templated":false,"search":{"key_":"sslnotafter.rb[\"{HOST.CONN}\",443]"},"hostids":'"$hostids"',"sortfield":"name"}'
-    upsert_graph "0 HTTPS Cert Remaining $num" $(echo "$JSON" | jq -r '.result[].itemid')
+    if [ "[]" != "$hostids" ]; then
+      zabbix_api item.get '{"output":"extend","templated":false,"search":{"key_":"sslnotafter.rb[\"{HOST.CONN}\",443]"},"hostids":'"$hostids"',"sortfield":"name"}'
+      upsert_graph "0 HTTPS Cert Remaining $num" $(echo "$JSON" | jq -r '.result[].itemid')
+    fi
 done
